90984937aa93d0213e46b37beb3ab134a77ac179
Palm Civet <palmcivet_email@qq.com>
2021-12-05 01:12:49 +0800

    :bug: Fix the base path

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import pkg from "../package.json";
+
+const homepage = pkg.homepage.split("/");
 
 export default defineConfig({
+  base: `/${homepage[homepage.length - 1]}/`,
   server: {
     port: 30000,
   },
